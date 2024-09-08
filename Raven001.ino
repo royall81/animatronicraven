1e92e275894961748a57ddb2eb61964580fd84ad
Roy Buijs <buijs.roed@gmail.com>
2024-09-08 19:40:24 +0200

    Small update

--- Raven001.ino
+++ Raven001.ino
@@ -1,7 +1,6 @@
 #include <ESP32Servo.h>
 
 Servo myservo;  // create servo object to control a servo
-// 16 servo objects can be created on the ESP32
 
 int pos = 0;    // variable to store the servo position
 // Recommended PWM GPIO pins on the ESP32 include 2,4,12-19,21-23,25-27,32-33 
