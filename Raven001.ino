4ad838c6ab7632728fe2422eb0ea414fb28b87c9
Roy Buijs <buijs.roed@gmail.com>
2024-09-11 22:29:52 +0200

    updated movement, outcommented serial when testing, saves last position

--- Raven001.ino
+++ Raven001.ino
@@ -6,11 +6,11 @@ Servo servoHeadBeak;  // create servo object to control a servo
 Servo servoWings;  // create servo object to control a servo
 Servo servoLegs;  // create servo object to control a servo
 
-int posHeadHor = 0;    // variable to store the servo position
-int posHeadVer = 0;    // variable to store the servo position
-int posHeadBeak = 0;    // variable to store the servo position
-int posWings = 0;    // variable to store the servo position
-int posLegs = 0;    // variable to store the servo position
+float posHeadHor;    // variable to store the servo position
+float posHeadVer;    // variable to store the servo position
+float posHeadBeak;    // variable to store the servo position
+float posWings;    // variable to store the servo position
+float posLegs;    // variable to store the servo position
 
 // Recommended PWM GPIO pins on the ESP32 include 2,4,12-19,21-23,25-27,32-33
 int servoPinHeadHor = 13;
@@ -23,6 +23,7 @@ int seqCounter;
 int move;
 float moveSmooth;
 float movePrev;
+float lastPosition;
 
 struct motorInstructions {
 	int startTime;
@@ -33,11 +34,13 @@ struct motorInstructions {
 };
 
 struct motorInstructions action[5] = {
-	// {5000, "servoHeadHor", 30, 0, false},
-	{10000, "servoHeadVer", 30, 0, false},
-	// {15000, "servoHeadBeak", 30, 0, false},
-	// {20000, "servoWings", 30, 0, false},
-	// {25000, "servoLegs", 30, 0, false}
+	{9000, "headHor", 1000, 1500, false},
+	{10000, "headVer", 1000, 1500, false},
+	{12000, "headHor", 2000, 0, false},
+	{12500, "headVer", 2000, 0, false},
+	//{15000, "beak", 30, 0, false},
+	// {20000, "wings", 30, 0, false},
+	// {25000, "legs", 30, 0, false}
 };
 
 void setup() {
@@ -57,9 +60,11 @@ void setup() {
 	// using default min/max of 1000us and 2000us
 	// different servos may require different min/max settings
 	// for an accurate 0 to 180 sweep
+	servoHeadHor.writeMicroseconds(1500);
+	servoHeadVer.writeMicroseconds(1500);
 
-	Serial.begin(9600);
-	Serial.println("Hello");
+	// Serial.begin(9600);
+	// Serial.println("Hello");
 }
 
 void loop() {
@@ -69,22 +74,22 @@ void loop() {
 	for (int i = 0; i < sizeof(action)/sizeof(action[0]); ++i) {
 		// if time is same as starttime or action is still active
 		if (seqCounter == action[i].startTime || action[i].active == true) {
-			Serial.print(action[i].startTime);
-			Serial.print(" , ");
-			Serial.print(action[i].servoName);
-			Serial.print(" , ");
-			Serial.print(action[i].distance);
-			Serial.print(" , ");
-			Serial.print(action[i].achieved);
-			Serial.print(" , ");
-			Serial.println(action[i].active);
+			// Serial.print(action[i].startTime);
+			// Serial.print(" , ");
+			// Serial.print(action[i].servoName);
+			// Serial.print(" , ");
+			// Serial.print(action[i].distance);
+			// Serial.print(" , ");
+			// Serial.print(action[i].achieved);
+			// Serial.print(" , ");
+			// Serial.println(action[i].active);
 			smoothMove(i);
 		}
 	}
 
 	// run loop 100 times a second
-	delay(10);
-	seqCounter = seqCounter + 10;
+	delay(20);
+	seqCounter = seqCounter + 20;
 
 	// for (pos; pos <= 100; pos += 1) { // goes from 0 degrees to 180 degrees
 	// in steps of 1 degree
@@ -99,15 +104,35 @@ void loop() {
 }
 
 void smoothMove(int actionId) {
+	// servo destination
 	int move = action[actionId].distance;
+	// Get latest servoPosition
+	if (action[actionId].servoName == "headHor") lastPosition = posHeadHor;
+	if (action[actionId].servoName == "headVer") lastPosition = posHeadVer;
+	if (action[actionId].servoName == "beak") lastPosition = posHeadBeak;
+	if (action[actionId].servoName == "wings") lastPosition = posWings;
+	if (action[actionId].servoName == "legs") lastPosition = posLegs;
+	if (lastPosition != 0 && action[actionId].active == false) {
+		action[actionId].achieved = lastPosition;
+	}
 	float movePrev = action[actionId].achieved;
 	moveSmooth = (move * 0.05) + (movePrev * 0.95);
 
-	Serial.print(moveSmooth);
-	Serial.print(" , ");
-	Serial.print(move);
-	Serial.print(" , ");
-	Serial.println(movePrev);
+	if (action[actionId].servoName == "headHor") servoHeadHor.writeMicroseconds(moveSmooth);
+	if (action[actionId].servoName == "headVer") servoHeadVer.writeMicroseconds(moveSmooth);
+	if (action[actionId].servoName == "beak") servoHeadBeak.writeMicroseconds(moveSmooth);
+	if (action[actionId].servoName == "wings") servoWings.writeMicroseconds(moveSmooth);
+	if (action[actionId].servoName == "legs") servoLegs.writeMicroseconds(moveSmooth);
+
+	// Serial.print(seqCounter);
+	// Serial.print(" , ");
+	// Serial.print(action[actionId].servoName);
+	// Serial.print(" , ");
+	// Serial.print(moveSmooth);
+	// Serial.print(" , ");
+	// Serial.print(move);
+	// Serial.print(" , ");
+	// Serial.println(movePrev);
 
 	// Set the move already has achieved
 	action[actionId].achieved = moveSmooth;
@@ -115,6 +140,11 @@ void smoothMove(int actionId) {
 	// If the move is completed
 	if (almostEqual(move, moveSmooth)) {
 		action[actionId].active = false;
+		if (action[actionId].servoName == "headHor") posHeadHor = action[actionId].distance;
+		if (action[actionId].servoName == "headVer") posHeadVer = action[actionId].distance;
+		if (action[actionId].servoName == "beak") posHeadBeak = action[actionId].distance;
+		if (action[actionId].servoName == "wings") posWings = action[actionId].distance;
+		if (action[actionId].servoName == "legs") posLegs = action[actionId].distance;
 	}
 	else {
 		action[actionId].active = true;
@@ -124,6 +154,6 @@ void smoothMove(int actionId) {
 }
 
 boolean almostEqual(float a, float b) {
-	const float DELTA = .01; // max difference before equal.
+	const float DELTA = 1; // max difference before equal.
 	return fabs(a - b) <= DELTA;
 }
